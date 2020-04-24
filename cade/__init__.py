d690570ebaf8da96ac9948bbfe4c4c729ba71f9b
vinid <chiccobia@gmail.com>
2020-04-24 19:45:42 +0200

    fixing version

--- cade/__init__.py
+++ cade/__init__.py
@@ -3,5 +3,5 @@
 """Top-level package for Temporal Word Embeddings with a Compass."""
 
 __author__ = """Federico Bianchi"""
-__email__ = 'federico.bianchi@unimib.it'
+__email__ = 'f.bianchi@unibocconi.it'
 __version__ = '0.1.0'
