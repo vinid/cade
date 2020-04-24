e00ee802c1f8ef889da43ce9c48a7470e1db6360
vinid <chiccobia@gmail.com>
2020-04-24 19:12:50 +0200

    fixing stuff for travis

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """The setup script."""
 
@@ -11,45 +10,40 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
-setup_requirements = [ ]
+setup_requirements = ['pytest-runner', ]
 
-test_requirements = [ ]
+test_requirements = ['pytest>=3', ]
 
 setup(
     author="Federico Bianchi",
-    author_email='federico.bianchi@unimib.it',
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    author_email='f.bianchi@unibocconi.it',
+    python_requires='>=3.5',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
-    description="Temporal Word Embeddings with a Compass",
-    entry_points={
-        'console_scripts': [
-            'cade=cade.cli:main',
-        ],
-    },
+    description="Contextualized Topic Models",
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
+    long_description_content_type='text/x-rst',
     include_package_data=True,
-    keywords='cade',
-    name='cade',
-    packages=find_packages(include=['cade']),
+    keywords='contextualized_topic_models',
+    name='contextualized_topic_models',
+    packages=find_packages(include=['cade', 'cade.*']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/vinid/cade',
-    version='0.1.0',
+    version='0.0.1',
     zip_safe=False,
 )
