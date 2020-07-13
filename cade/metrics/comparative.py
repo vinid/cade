ec74a4f8098ebe353426d8e8d0def5de4a1a3c08
belerico <belo.fede@outlook.com>
2020-07-13 21:33:53 +0000

    Fix intersection_nn to be betwenn0 and 1 (Higher means no change)

--- cade/metrics/comparative.py
+++ cade/metrics/comparative.py
@@ -4,6 +4,17 @@ import math
 import numpy as np
 from scipy.spatial.distance import cosine
 
+avg_m1 = 0
+avg_m2 = 0
+initialized_avgs = False
+
+
+def initialize_avgs(m1, m2):
+    global avg_m1, avg_m2, initialized_avgs
+    avg_m1 = np.average(m1[m1.wv.vocab], axis=0)
+    avg_m2 = np.average(m2[m2.wv.vocab], axis=0)
+    initialized_avgs = True
+
 
 def get_neighbors_set(word, slice_year, topn):
     return set([k[0] for k in slice_year.wv.most_similar(word, topn=topn)])
@@ -31,6 +42,8 @@ def lncs2_setted(word, m1, m2, topn):
     :return:
     """
 
+    global avg_m1, avg_m2, initialized_avgs
+
     words_m1 = list(get_neighbors_set(word, m1, topn))
     words_m2 = list(get_neighbors_set(word, m2, topn))
 
@@ -38,29 +51,27 @@ def lncs2_setted(word, m1, m2, topn):
 
     vec_1 = []
     vec_2 = []
-    avg = 0
-    mean = False
+    mean = False or initialized_avgs
 
     for inner_word in all_words:
         if inner_word in m1.wv.vocab:
             vec_1.append(1 - cosine(m1.wv[word], m1.wv[inner_word]))
         else:
             if not mean:
-                avg = np.average(m1[m1.wv.vocab], axis=0)
+                avg_m1 = np.average(m1[m1.wv.vocab], axis=0)
                 mean = True
-            vec_1.append(1 - cosine(m1.wv[word], avg))
+            vec_1.append(1 - cosine(m1.wv[word], avg_m1))
 
-    avg = 0
-    mean = False
+    mean = False or initialized_avgs
 
     for inner_word in all_words:
         if inner_word in m2.wv.vocab:
             vec_2.append(1 - cosine(m2.wv[word], m2.wv[inner_word]))
         else:
             if not mean:
-                avg = np.average(m2[m2.wv.vocab], axis=0)
+                avg_m2 = np.average(m2[m2.wv.vocab], axis=0)
                 mean = True
-            vec_2.append(1 - cosine(m2.wv[word], avg))
+            vec_2.append(1 - cosine(m2.wv[word], avg_m2))
 
     return 1 - cosine(vec_1, vec_2)
 
@@ -76,13 +87,14 @@ def lncs2(word, m1, m2, topn):
     :return:
     """
 
+    global avg_m1, avg_m2, initialized_avgs
+
     words_m1 = list(get_neighbors_set(word, m1, topn))
     words_m2 = list(get_neighbors_set(word, m2, topn))
 
     vec_1 = []
     vec_2 = []
-    avg = 0
-    mean = False
+    mean = False or initialized_avgs
 
     # Cosine similarity between "word" and every word in its m1-neighbour
     # within the m1 space
@@ -95,14 +107,13 @@ def lncs2(word, m1, m2, topn):
         if wtest not in m1.wv.vocab:
             if not mean:
                 # Represent OOV words in m1 space empirically with its mean
-                avg = np.average(m1[m1.wv.vocab], axis=0)
+                avg_m1 = np.average(m1[m1.wv.vocab], axis=0)
                 mean = True
-            vec_1.append(1 - cosine(m1[word], avg))
+            vec_1.append(1 - cosine(m1[word], avg_m1))
         else:
             vec_1.append(1 - cosine(m1.wv[word], m1.wv[wtest]))
 
-    avg = 0
-    mean = False
+    mean = False or initialized_avgs
 
     # Cosine similarity between "word" and every word in its m1-neighbour
     # within the m2 space
@@ -110,9 +121,9 @@ def lncs2(word, m1, m2, topn):
         if wtest not in m2.wv.vocab:
             if not mean:
                 # Represent OOV words in m1 space empirically with its mean
-                avg = np.average(m2[m2.wv.vocab], axis=0)
+                avg_m2 = np.average(m2[m2.wv.vocab], axis=0)
                 mean = True
-            vec_2.append(1 - cosine(m2[word], avg))
+            vec_2.append(1 - cosine(m2[word], avg_m2))
         else:
             vec_2.append(1 - cosine(m2.wv[word], m2.wv[wtest]))
 
@@ -167,4 +178,4 @@ def intersection_nn(word, m1, m2, topn=1000):
     words_m1 = get_neighbors_set(word, m1, topn)
     words_m2 = get_neighbors_set(word, m2, topn)
     intersection = words_m1.intersection(words_m2)
-    return 1 - (len(intersection) / topn)
+    return len(intersection) / topn
